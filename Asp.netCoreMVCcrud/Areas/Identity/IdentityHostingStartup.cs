7632a28ab2b7674c6e6eb502508d92cd575e8f4a
Manuel Phetla <mnockz@gmail.com>
2019-08-18 19:45:42 +0200

    Added identity using scafold

---
+++ Asp.netCoreMVCcrud/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Asp.netCoreMVCcrud.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Asp.netCoreMVCcrud.Areas.Identity.IdentityHostingStartup))]
+namespace Asp.netCoreMVCcrud.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DevConnection>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DevConnection")));
+
+                services.AddDefaultIdentity<DevConnectionUser>()
+                    .AddEntityFrameworkStores<DevConnection>();
+            });
+        }
+    }
+}
